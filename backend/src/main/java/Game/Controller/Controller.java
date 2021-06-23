c07434a69bd73463e4295760ec05cbdcd0cd4a08
Nasser-Eddin-Nasser <eddinnasser@live.com>
2021-06-23 10:40:33 +0200

    implement api controller in BE

--- backend/src/main/java/Game/Controller/Controller.java
+++ backend/src/main/java/Game/Controller/Controller.java
@@ -1,26 +1,21 @@
 package Game.Controller;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+ 
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/controller")
 @RestController
 public class Controller {
 
-    @GetMapping("/")
-    public String getAll() {
-        return "im alive";
-    }
-
-
+ 
     @PostMapping("/startNewGame")
-    public void createApplicant( ) {
-        System.out.println("User started new Game!");
+    public void startNewGame( ) {
+        System.out.println("User started new Game!"); 
+    }
+    
+    @PostMapping("/score")
+    public void score(@RequestBody String payload) { 
+        System.out.println(payload); 
     }
 }
+
+
