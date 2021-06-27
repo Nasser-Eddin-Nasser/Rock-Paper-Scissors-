d82a2e11d7904d1312692e4a74548116ef3afd0a
Nasser-Eddin-Nasser <eddinnasser@live.com>
2021-06-27 18:44:46 +0200

    add CrossOrigin

--- backend/src/main/java/Game/Controller/Controller.java
+++ backend/src/main/java/Game/Controller/Controller.java
@@ -1,18 +1,17 @@
 package Game.Controller;
 
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import Model.Score;
+import Game.Model.Score;
+ 
 
 @RequestMapping("api/controller")
 @RestController
 public class Controller {
-
-	@PostMapping("/startNewGame")
+	@CrossOrigin
+	@GetMapping("/startNewGame")
 	public ResponseEntity<String> startNewGame() {
 		try {
 			System.out.println("startNewGame");
@@ -23,13 +22,15 @@ public class Controller {
 
 	}
 
+	@CrossOrigin
 	@PostMapping("/score")
-	public ResponseEntity<Score> score(@RequestBody Score score) { 
-			System.out.println(score);  
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(score);
-		 
+	public ResponseEntity<Score> score(@RequestBody Score score) {
+		System.out.println(score);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(score);
+
 	}
 
+	@CrossOrigin
 	@GetMapping("/error")
 	public ResponseEntity<String> sendError() {
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Exception");
